9523575a5cd14beaf1dcb1edd2aaadef3d4e0942
jayak <kumarijaya127@gmail.com>
2024-05-18 23:18:50 +0530

    removed node_modules

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-const app = require("express");
+const express = require("express");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(jwt());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening to port on ${PORT}`);
+});
