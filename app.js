7907ee836e5d9b3b641e0323c490f3adc5f06488
jayak <kumarijaya127@gmail.com>
2024-05-19 18:33:53 +0530

    add .env file

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const path = require("path");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(jwt());
+app.use(cookieParser());
 
-const PORT = 3000;
+app.get("/", (req, res) => {
+  res.cookie("userName", "cookiename");
+  res.send("welcome to express");
+});
+
+var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`listening to port on ${PORT}`);
